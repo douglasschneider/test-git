bc6f4e9290f0e4fb1ff3ec7a678d4c74deed2f36
Aprendendo Sobre <aprendendodouglas@outlook.com>
2023-01-15 02:31:41 -0300

    Pergunta a idade da pessoa

---
+++ src/idade.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int idade;
+	cout << "Digite sua idade: ";
+	cin >> idade;
+	cout << "Você tem " << idade << " anos de idade." << endl;
+
+	return 0;
+}
